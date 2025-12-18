a68c090bbc0dd3cb91c09bfa987f54f028ce92c9
Jude Osafo <judeyawosafo1473@gmail.com>
2025-12-18 13:12:10 -0500

    Add snowfall effect and reinstall deps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Snowfall from "react-snowfall";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
 import Events from "@/pages/Events";
@@ -48,6 +49,16 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <Snowfall
+          snowflakeCount={150}
+          style={{
+            position: "fixed",
+            width: "100vw",
+            height: "100vh",
+            pointerEvents: "none",
+            zIndex: 10,
+          }}
+        />
         <Toaster />
         <Router />
       </TooltipProvider>
