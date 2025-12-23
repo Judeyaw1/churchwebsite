c99b88bd8efbcec9b121feeaefca9f4a5f7f429c
Jude Osafo <judeyawosafo1473@gmail.com>
2025-12-23 18:48:50 -0500

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -32,15 +32,6 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "vendor-react";
-            }
-            if (id.includes("framer-motion")) {
-              return "vendor-motion";
-            }
-            if (id.includes("lucide-react")) {
-              return "vendor-icons";
-            }
             return "vendor";
           }
         },
